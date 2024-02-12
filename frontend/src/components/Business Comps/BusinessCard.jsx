285154ea6cb148aae7c79d36fe6fb6a159e1328c
Daniel Morley <morleydan10@gmail.com>
2024-02-12 00:46:12 -0500

    css for browse users completed

--- frontend/src/components/Business Comps/BusinessCard.jsx
+++ frontend/src/components/Business Comps/BusinessCard.jsx
@@ -13,13 +13,16 @@ function BusinessCard ({ business, viewBusinessProfile }) {
     // const handleClickBusiness = (e) => navigate('')
 
     return(
-        <div class="card">
-        <img src={business.profile_pic_url} class="card-img-top" alt="Business Picture"/>
-        <div class="card-body">
-            <h5 class="card-title">{business.name}</h5>
-            <div class="container">
-                <button onClick={() => viewBusinessProfile(business)}>View Profile</button>
-                </div>
+        <div className="card">
+            <div className='card-img-div'>
+                <img src={business.profile_pic_url} className="card-img" alt="Business Picture"/>
+            </div>
+        <div className="card-body">
+                <h2 className="card-title">{business.name}</h2>
+                <h4 className="card-subtitle">{business.city}</h4>
+                <h4 className="card-subtitle">{business.type}</h4>
+                <button className="view-button" onClick={() => viewBusinessProfile(business)}>View Profile</button>
+                
             </div>
         </div>
         // <Card className="card">
