c08f2f952667cbf43edfa37969f4e11b34fcc7c2
Daniel Morley <morleydan10@gmail.com>
2024-02-13 14:34:44 -0500

    css adjustments to tables, adjusted rules for fetch requests

--- frontend/src/components/Business Comps/YourRequestsTable.jsx
+++ frontend/src/components/Business Comps/YourRequestsTable.jsx
@@ -47,46 +47,47 @@ function YourRequestsTable(){
 
     return(
         <div className="your-requests-table">
-            <h2 className="portfolio-header"> Your Requests</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Request Number</th>
-                        <th>Business</th>
-                        <th>Description</th>
-                        <th>Date Created</th>
-                        <th>Compensation</th>
-                        <th>Artist</th>
-                        <th>Open/Closed</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {yourRequests.map((yourRequest) => (
-                    <tr key={yourRequest.id}>
-                        <td>{yourRequest.id}</td>
-                        <td>{yourRequest.business.name}</td>
-                        <td>{yourRequest.description}</td>
-                        <td>{yourRequest.date_created}</td>
-                        <td>{yourRequest.compensation}</td>
-                        {/* To add Navlink to artist's profile, need to solve authentication issues */}
-                        <td>{ yourRequest.artist  ? (yourRequest.artist.name) : ('')}</td>
-                        <td>{ yourRequest.artist ? ('Closed') : (
-                            <>
-                                <button onClick={() => {
-                                    setIsPopupOpen(true)}}>View Bids</button>
-                                <BidsPopup trigger={isPopupOpen} setTrigger={setIsPopupOpen} yourRequestId={yourRequest.id} handleUpdateRequest={handleUpdateRequest} />
-                            </>
-                        )}</td>
-                        <td>
-                            <button className="delete-button" onClick={(e) => handleDeleteYourRequest(e, yourRequest)}>
-                                ❌
-                            </button>
-                        </td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Request Number</th>
+                            <th>Business</th>
+                            <th>Description</th>
+                            <th>Date Created</th>
+                            <th>Compensation</th>
+                            <th>Artist</th>
+                            <th>Open/Closed</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {yourRequests.map((yourRequest) => (
+                        <tr key={yourRequest.id}>
+                            <td>{yourRequest.id}</td>
+                            <td>{yourRequest.business.name}</td>
+                            <td>{yourRequest.description}</td>
+                            <td>{yourRequest.date_created}</td>
+                            <td>{yourRequest.compensation}</td>
+                            {/* To add Navlink to artist's profile, need to solve authentication issues */}
+                            <td>{ yourRequest.artist  ? (yourRequest.artist.name) : ('')}</td>
+                            <td>{ yourRequest.artist ? ('Closed') : (
+                                <>
+                                    <button onClick={() => {
+                                        setIsPopupOpen(true)}}>View Bids</button>
+                                    <BidsPopup trigger={isPopupOpen} setTrigger={setIsPopupOpen} yourRequestId={yourRequest.id} handleUpdateRequest={handleUpdateRequest} />
+                                </>
+                            )}</td>
+                            <td>
+                                <button className="delete-button" onClick={(e) => handleDeleteYourRequest(e, yourRequest)}>
+                                Delete
+                                </button>
+                            </td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
