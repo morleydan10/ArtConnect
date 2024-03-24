bad7944f3bd745bb40cb0451141dc1d47340fa67
Daniel Morley <morleydan10@gmail.com>
2024-03-24 18:49:05 -0400

    edited carousel, styling on homepage, changed primary font

--- frontend/src/components/CarouselSlider.jsx
+++ frontend/src/components/CarouselSlider.jsx
@@ -6,11 +6,11 @@ import { NavLink } from "react-router-dom";
 function CarouselSlider({ artistUser }){
 
     const settings = {
-        autoplaySpeed: 3000,
+        autoplaySpeed: 300,
         dots: true,
-        infinite: false,
+        infinite: true,
         centerMode: true,
-        speed: 300,
+        speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
     }
@@ -19,30 +19,45 @@ function CarouselSlider({ artistUser }){
         <Slider {...settings} className="slider">
 
             <div id="slider-slide-1">
-                <p>Welcome to our platform for connecting artists and businesses!</p>
-                <p>Finish setting up your profile now!</p>
+                <h2 className="homepage-feature-header">Welcome to our platform for connecting artists and businesses!</h2>
+                <p className="set-up-text">Finish setting up your profile now!</p>
                 {artistUser ? (
                     <NavLink to="/ArtistProfile">
-                        <a to="/ArtistProfile" className="get-started-button">Get Started</a>
+                        <button className="call-to-action">Get Started</button>
                     </NavLink>
                 ):(
                     <NavLink to="/BusinessProfile">
-                        <a to="/BusinessProfile" className="get-started-button">Get Started</a>
+                        <button className="call-to-action">Get Started</button>
                     </NavLink>
                 )
                 }
             </div>
             <div id="slider-slide-2">
-                <h2 className="homepage-feature-header">Connect and Collaborate</h2>
-                <p>Connect with artists and start creative collaborations.</p>
+                <h2 className="homepage-feature-header">Find Artists</h2>
+                <p className="set-up-text">Discover talented artists right in your neighborhood.</p>
+                <NavLink to="/BrowseUsers">
+                    <button className="call-to-action">View Artists</button>
+                </NavLink>
             </div>
             <div id="slider-slide-3">
-                <h2 className="homepage-feature-header">Find Artists</h2>
-                <p>Discover talented artists right in your neighborhood.</p>
-            </div>
-            <div id="slider-slide-4">
-                <h2 className="homepage-feature-header">Post Requests</h2>
-                <p>Businesses can post creative work requests.</p>
+                {artistUser ? (
+                    <>
+                        <h2 className="homepage-feature-header">Apply to Open Requests</h2>
+                        <p className="set-up-text">Apply to open requests and get the ball rolling today</p>
+                        <NavLink to="/OpenRequests">
+                            <button className="call-to-action">View Open Requests</button>
+                        </NavLink>
+                    </>
+                ):(
+                    <>
+                        <h2 className="homepage-feature-header">Post Requests</h2>
+                        <p className="set-up-text">Businesses can post creative work requests.</p>
+                        <NavLink to="/OpenRequests">
+                            <a to="/OpenRequests" className="call-to-action">View Open Requests</a>
+                        </NavLink>
+                    </>
+                )}
+                
             </div>
         </Slider>
 
